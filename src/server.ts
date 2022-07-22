76a1e26581f6c6be72f7c3982f10c49173b48c27
ouwargui <guikemon2@gmail.com>
2022-07-22 15:22:57 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
 import 'dotenv/config';
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import {checkEnvs} from './config/env';
-import router from './routes';
+import routes from './routes';
 
 checkEnvs();
 
 const app = express();
 app.use(express.json());
-app.use(router);
+app.use(routes);
 
-app.get('/', (req, res) => res.send('hello world'));
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res
+    .status(500)
+    .json({status: 'error', message: 'Internal server error'});
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
